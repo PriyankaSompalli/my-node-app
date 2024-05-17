4a77319459ec6ea65edfb4e74fecc5e77419cdd1
Priyanka Sompalli <sompallipriyanka2001@gmail.com>
2024-05-17 13:25:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const Book = require('./models/book');
+
+
+app.post('/books', async (req, res) => {
+  const book = new Book(req.body);
+  try {
+    await book.save();
+    res.status(201).send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.status(200).send(books);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).send();
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+
+app.patch('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!book) {
+      return res.status(404).send();
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (!book) {
+      return res.status(404).send();
+    }
+    res.send(book);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
